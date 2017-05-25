807d7e7cf4ace7adbe7320a8a0b57bc01a054ca1
thatscrazy <thatscrazy@github.com>
2017-05-25 14:47:07 -0400

    time to build testing, cleared out scrap code

--- backup/notes.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'cards.rb'
-
-# how to figure out the value of hands ?
-
-SUITS.each { | suit | puts suit }
-
-
--- backup/test_hands.rb
+++
@@ -1,7 +0,0 @@
-game = Cards::CardGame.new
-
-player = game.create_hand(2)
-board = game.create_hand(5)
-
-player_hand = ( player + board ).sort
-
